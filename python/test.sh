d9303977adcfcb9db072e675d4f6327b0e078043
Kotaro-Bannai <92736992+Kotaro-Bannai@users.noreply.github.com>
2022-05-18 00:43:02 +0900

    Update test.sh

--- python/test.sh
+++ python/test.sh
@@ -2,7 +2,7 @@
 
 TESTPYTHON="main.py"
 
-(sleep 10 ; ps a | grep Python | grep $TESTPYTHON | tr -s " " | sed -e 's/^ //g' | cut -d " " -f 1 | xargs kill -TERM) &
+(sleep 10 ; ps aux | grep Python | grep $TESTPYTHON | tr -s " " | sed -e 's/^ //g' | cut -d " " -f 1 | xargs kill -TERM) &
 python3 $TESTPYTHON
 
 exit 0
